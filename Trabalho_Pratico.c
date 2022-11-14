e692ad65a86f80123c6519ac25a1e4d5457f0b29
Jojow-World <jonatas-fab@hotmail.com>
2022-11-14 11:50:48 +0000

    first

---
+++ Trabalho_Pratico.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>   
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+
+
+void abertura(){
+
+    printf("****************************************************\n");
+    printf("* Bem vindo ao registo referencias bibliograficas *\n");
+    printf("****************************************************\n");
+}
+
+
+
+int main(){
+
+abertura();
+
+
+}
