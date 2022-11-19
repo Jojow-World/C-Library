5a7b636efe97bcde03cd63e7a9fd87da82c1d817
Jojow-World <jonatas-fab@hotmail.com>
2022-11-19 03:49:16 +0000

    Trabalho_Pratico.c

--- Trabalho_Pratico.c
+++ Trabalho_Pratico.c
@@ -5,19 +5,49 @@
 #include <time.h>
 
 
+typedef struct livros
+{
+    char nome[50];
+};
+
+
+
 void abertura(){
 
     printf("****************************************************\n");
     printf("* Bem vindo ao registo referencias bibliograficas *\n");
-    printf("****************************************************\n");
+    printf("****************************************************\n\n");
+
+    system("pause"); // tem um pause
+    system("cls"); // limpa a tela e segue pra outra linha de codigo
+
 }
 
 
 
 int main(){
 
+
+
 abertura();
 
+    struct livros publicacao;
+    
+
+    printf()
 
 
-}
+    
+    printf("Nome da publicacao : ");
+    fgets(publicacao.nome, 50, stdin); // stdin pega do teclado igual scanf
+
+    printf("Nome do livro : %s", publicacao.nome);
+
+
+
+		
+	}
+	
+
+
+
